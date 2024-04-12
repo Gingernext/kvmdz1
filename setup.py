4eb48f9327bc7068acaf438b1d225e3f9d8f6504
Gingernext <166530228+Gingernext@users.noreply.github.com>
2024-04-12 21:36:44 +0800

    Update setup.py
    rp2040 v0.1

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ def main() -> None:
 
     setup(
         name="kvmd",
-        version="3.332",
+        version="0.1",
         url="https://github.com/pikvm/kvmd",
         license="GPLv3",
         author="Maxim Devaev",
