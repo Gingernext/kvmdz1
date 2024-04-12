6781f1cad4d2d95a09984313846579218a7ec706
Gingernext <166530228+Gingernext@users.noreply.github.com>
2024-04-12 21:48:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ def main() -> None:
 
     setup(
         name="kvmd",
-        version="0.1",
+        version="0.2",
         url="https://github.com/pikvm/kvmd",
         license="GPLv3",
         author="Maxim Devaev",
